77d41d688ac9aca1ebf9167d4a1b3a02f2c4b367
Kus Cámara <kcmr@users.noreply.github.com>
2018-03-31 03:18:16 +0200

    refactor(tasks): move style tasks to a separate gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const path = require('path');
 const gulp = require('gulp');
 const del = require('del');
 const browserSync = require('browser-sync').create();
-const processInline = require('gulp-process-inline');
 const inlineSource = require('gulp-inline-source');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
@@ -12,41 +10,32 @@ const pxtorem = require('postcss-pixels-to-rem');
 const prettify = require('gulp-html-prettify');
 const stylelint = require('stylelint');
 const reporter = require('postcss-reporter');
-const reporterFormatter = require('postcss-reporter/lib/formatter');
-const component = path.basename(__dirname);
 const config = require('./.buildconfig.json');
 
 const BUILD_DIRECTORY = 'dist/';
+const TMP_DIRECTORY = '.tmp/';
 
 gulp.task('clean', () => del([BUILD_DIRECTORY]));
 
-gulp.task('build:dist', ['clean'], () => {
-  const files = processInline();
-  const customFormatter = reporterFormatter();
-
-  return gulp.src(['src/*.html'])
-    .pipe(inlineSource({
-      compress: false,
-      swallowErrors: true,
-    }))
-    .pipe(files.extract('style'))
+gulp.task('styles', () => {
+  return gulp.src(['src/*.css'])
     .pipe(postcss([
       pxtorem(config.pxtorem),
       stylelint(),
       reporter({
         clearReportedMessages: true,
-        formatter: function(params) {
-          /* eslint-disable no-console */
-          console.log(customFormatter({
-            source: `src/${component}.css`,
-            messages: params.messages,
-          }));
-          /* eslint-enable no-console */
-        },
       }),
       autoprefixer(config.autoprefixer),
     ]))
-    .pipe(files.restore())
+    .pipe(gulp.dest(TMP_DIRECTORY));
+});
+
+gulp.task('build:dist', ['clean', 'styles'], () => {
+  return gulp.src(['src/*.html'])
+    .pipe(inlineSource({
+      compress: false,
+      swallowErrors: true,
+    }))
     .pipe(prettify(config.prettify))
     .pipe(gulp.dest(BUILD_DIRECTORY));
 });
@@ -56,7 +45,7 @@ gulp.task('start-browsersync', () => {
 });
 
 gulp.task('watch:sources', () => {
-  gulp.watch(['src/*'], ['build:dist']);
+  gulp.watch(['src/*'], ['styles', 'build:dist']);
 });
 
 gulp.task('watch:dist', () => {
